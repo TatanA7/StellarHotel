d510571ad058a936dcadb9a76fde7667d9aab382
Jhonathan Ascencio <jeascencio@cognox.com>
2024-10-21 20:26:46 -0500

    deploy errors2

--- src/main.ts
+++ src/main.ts
@@ -8,29 +8,9 @@ import * as csrf from 'csurf'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
-  app.use(cookieParser());
-  const csrfProtection = csrf({ cookie: true });
-  app.use(csrfProtection);
-
-  // Middleware para enviar el token CSRF en las cookies
-  app.use((req, res, next) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken(), {
-      httpOnly: false, // Permitir acceso al token desde el cliente
-      secure: process.env.NODE_ENV === 'production', // Solo si estás en producción
-    });
-    next();
-  });
   app.enableCors({
-    origin: ['https://stellarhotel.onrender.com'],  // o cualquier dominio que estés usando
+    origin: ['https://stellarhotel.onrender.com','http://localhost:3000'],  // o cualquier dominio que estés usando
     credentials: true,  // Si estás usando cookies o autenticación
   });
   app.listen(port, () => {
